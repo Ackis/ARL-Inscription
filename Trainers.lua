cb79d54217a05e49ecec784443c0a553a7c9ac0c
Ressy <resa1983@hotmail.com>
2014-07-26 19:51:04 -0400

    Add Trainer & add WoD crafts

--- Trainers.lua
+++ Trainers.lua
@@ -43,6 +43,7 @@ function module:InitializeTrainers()
 	addon:AddTrainer(62327, "Scribe Rinji", Z.THE_JADE_FOREST, 47.6, 35, "Neutral")
 	addon:AddTrainer(64691, "Lorewalker Huynh", Z.VALE_OF_ETERNAL_BLOSSOMS, 81.9, 29.3, "Neutral")
 	addon:AddTrainer(66355, "Master Marshall", Z.KUN_LAI_SUMMIT, 50.6, 42.2, "Neutral")
+	addon:AddTrainer(85911, "Sen Xia", Z.STORMSHIELD, 63.0, 33.7, "Alliance")
 
 	self.InitializeTrainers = nil
 end
