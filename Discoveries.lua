f6c13a356efe61986f88961ce0230883362fee1c
Ressy <resa1983@hotmail.com>
2014-10-12 16:23:41 -0400

    Reorder Discoveries & add Warbinder

--- Discoveries.lua
+++ Discoveries.lua
@@ -21,16 +21,17 @@ function module:InitializeDiscoveries()
 	end
 
 	AddDiscovery("DISCOVERY_INSC_AUTOLEARN")
-	AddDiscovery("DISCOVERY_INSC_DREAMS")
-	AddDiscovery("DISCOVERY_INSC_LIONS")
 	AddDiscovery("DISCOVERY_INSC_BLACKFALLOW")
-	AddDiscovery("DISCOVERY_INSC_SEA")
 	AddDiscovery("DISCOVERY_INSC_CELESTIAL")
-	AddDiscovery("DISCOVERY_INSC_MIDNIGHT")
+	AddDiscovery("DISCOVERY_INSC_DREAMS")
 	AddDiscovery("DISCOVERY_INSC_ETHEREAL")
-	AddDiscovery("DISCOVERY_INSC_MOONGLOW")
 	AddDiscovery("DISCOVERY_INSC_JADEFIRE")
+	AddDiscovery("DISCOVERY_INSC_LIONS")
+	AddDiscovery("DISCOVERY_INSC_MIDNIGHT")
+	AddDiscovery("DISCOVERY_INSC_MOONGLOW")
+	AddDiscovery("DISCOVERY_INSC_SEA")
 	AddDiscovery("DISCOVERY_INSC_SHIMMERING")
+	AddDiscovery("DISCOVERY_INSC_WARBINDER")
 
 	self.InitializeDiscoveries = nil
 end
