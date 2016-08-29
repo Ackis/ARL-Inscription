0aca5863b739369632cadc5aebdf94db43fb8df3
Ressy <resa1983@hotmail.com>
2016-08-28 20:34:50 -0400

    Add mobs

--- MobDrops.lua
+++ MobDrops.lua
@@ -36,6 +36,7 @@ function module:InitializeMobDrops()
 		})
 	end
 
+	AddMob(17380,	BN.BROGGOK,				Z.THE_BLOOD_FURNACE,		0.0, 0.0)
 	AddMob(26679,	L["Silverbrook Trapper"],		Z.GRIZZLY_HILLS,		27.3, 33.9)
 	AddMob(26708,	L["Silverbrook Villager"],		Z.GRIZZLY_HILLS,		27.3, 33.9)
 	AddMob(27546,	L["Silverbrook Hunter"],		Z.GRIZZLY_HILLS,		37.5, 68.0)
@@ -52,7 +53,9 @@ function module:InitializeMobDrops()
 	AddMob(105393,	BN.ILGYNOTH__HEART_OF_CORRUPTION,	Z.THE_EMERALD_NIGHTMARE,	0.0, 0.0)
 	AddMob(105503,	BN.GULDAN,				Z.THE_NIGHTHOLD,		0.0, 0.0)
 	AddMob(107699,	BN.SPELLBLADE_ALURIEL,			Z.THE_NIGHTHOLD,		0.0, 0.0)
+	AddMob(109943,	BN.ANA_MOUZ,				Z.SURAMAR,			0.0, 0.0) --Needs updating
 	AddMob(110965,	BN.GRAND_MAGISTRIX_ELISANDE,		Z.THE_NIGHTHOLD,		0.0, 0.0)
+	AddMob(111057,	L["The Rat King"],			Z.THE_ARCWAY,			0.0, 0.0)
 	AddMob(113534,	BN.CENARIUS,				Z.THE_EMERALD_NIGHTMARE,	0.0, 0.0)
 
 	self.InitializeMobDrops = nil
