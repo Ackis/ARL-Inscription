63cae895024f211c345b3e44dc00f9d3f349c12f
Ressy <resa1983@hotmail.com>
2016-08-28 20:36:32 -0400

    Add Vendors

--- Vendors.lua
+++ Vendors.lua
@@ -36,11 +36,14 @@ function module:InitializeVendors()
 	end
 
 	AddVendor(14847,	L["Professor Thaddeus Paleo"],		Z.DARKMOON_ISLAND,		51.8,	60.6,	"Neutral")
+	AddVendor(18072,	L["Elementalist Sharvak"],		Z.NAGRAND_OUTLAND,		60.6,	22.2,	"Neutral")
 	AddVendor(77372,	L["Eric Broadoak"],			Z.LUNARFALL,			 0.0,	 0.0,	"Alliance") -- Alliance Garrison
 	AddVendor(79829,	L["Urgra"],				Z.FROSTWALL,			 0.0,	 0.0,	"Horde") -- Horde Garrison
 	AddVendor(87063,	L["Joao Calhandro"],			Z.STORMSHIELD,			63.0,	34.4,	"Alliance") -- Alliance Ashran
 	AddVendor(87551,	L["Maru'sa"],				Z.WARSPEAR,			76.0,	48.6,	"Horde") -- Horde Ashran
 	AddVendor(93544,	L["Jang Quillpaw"],			Z.DALARAN_BROKENISLES,		41.2,	36.8,	"Neutral")
+	AddVendor(94100,	L["Lucas Sigmund"],			Z.HIGHMOUNTAIN,			40.0,	52.4,	"Neutral")
+	AddVendor(106887,	L["Cravitz Lorent"],			Z.DALARAN_BROKENISLES,		47.0,	28.8,	"Neutral")
 
 	self.InitializeVendors = nil
 end
